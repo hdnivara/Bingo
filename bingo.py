f1d66438c128b7ca65a16dcdcacb58af0d28c52d
Aravindhan Dhanasekaran <aradhana@cisco.com>
2017-03-28 15:15:06 -0700

    bingo: Bing! search few randome news headlines
    Do a Bing! search on a number of news headlines. The news headlines
    are fetched from NewsAPI service.

---
+++ bingo.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+
+"""
+Use Bing! to search news headlines.
+"""
+
+
+import argparse
+import pprint
+import random
+import time
+import webbrowser
+import requests
+
+SEARCH_MAX = 32
+BING_URL = "https://www.bing.com/"
+SEARCH_URL = "https://www.bing.com/search?q="
+
+API_URL = "https://newsapi.org/v1/articles?apiKey="
+API_SRC = "&source="
+API_KEY = None
+
+NEWS_SOURCES = [
+    "abc-news-au",
+    "ars-technica",
+    "associated-press",
+    "bbc-news",
+    "bbc-sport",
+    "bloomberg",
+    "cnbc",
+    "cnn",
+    "engadget",
+    "espn",
+    "espn-cric-info",
+    "fortune",
+    "google-news",
+    "hacker-news",
+    "independent",
+    "national-geographic",
+    "new-york-magazine",
+    "recode",
+    "reddit-r-all",
+    "sky-news",
+    "sky-sports-news",
+    "techcrunch",
+    "the-economist",
+    "the-guardian-uk",
+    "the-hindu",
+    "the-new-york-times",
+]
+
+def __parse_args():
+    parser = argparse.ArgumentParser("Bing! few news headlines.")
+
+    parser.add_argument("searches", type=int, nargs='?', default=4,
+                        help="number of Bing!s to be done")
+    parser.add_argument("-k", "--key", type=str, required=True,
+                        help="NewsAPI key")
+    return parser.parse_args()
+
+
+def __news_get(source):
+    news_url = API_URL + API_KEY + API_SRC + source
+
+    try:
+        print news_url
+        resp = requests.get(news_url)
+    except requests.exceptions.RequestException:
+        print "Fetching news feeds failed."
+
+    news_data = resp.json()
+    if news_data['status'] == 'error':
+        raise ValueError(news_data['message'])
+
+    return news_data['articles']
+
+
+def __news_desc_get(source):
+    news = __news_get(source)
+
+    if news is None:
+        return None
+
+    news_headlines = []
+    for each_article in news:
+        news_headlines.append(each_article['description'])
+
+    return news_headlines
+
+
+def bing_search(search_query):
+    """ Do a Bing! search on the incoming query. """
+    search_query = search_query.encode(encoding="utf-8")
+    query = SEARCH_URL + str(search_query).replace(" ", "%20")
+    webbrowser.open_new_tab(query)
+    print query
+
+
+def do_bing_search(num_searches):
+    """ Do 'n' Bing! searches on a random news articles. """
+    if num_searches == 0:
+        return
+
+    webbrowser.open(BING_URL, new=1, autoraise=True)
+    while num_searches:
+        news_src = random.choice(NEWS_SOURCES)
+
+        news = __news_desc_get(news_src)
+        if news is None:
+            # Fetching news from that source failed. Try another source.
+            continue
+
+        for each_article in news:
+            bing_search(each_article)
+
+            num_searches -= 1
+            if num_searches == 0:
+                break
+
+            time.sleep(2)
+
+
+def main():
+    global API_KEY
+
+    args = __parse_args()
+    API_KEY = args.key
+    do_bing_search(int(args.searches))
+
+
+if __name__ == "__main__":
+    main()
